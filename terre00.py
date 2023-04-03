456964615b8f491b66bce0319116229cd46d4945
EnesBrt <enes.barut@gmx.com>
2023-04-03 15:47:59 +0200

    Add terre00.py

---
+++ terre00.py
@@ -0,0 +1,10 @@
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
+           "w", "x", "y", "z"]
+
+
+def alphabet(letters_list):
+    for n in letters_list:
+        print(n)
+
+
+alphabet(letters)
