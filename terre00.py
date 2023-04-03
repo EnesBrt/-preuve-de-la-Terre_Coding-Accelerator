6827c6cedcbb0da3295ac4f58caa9a69038535c4
EnesBrt <enes.barut@gmx.com>
2023-04-03 16:44:24 +0200

    updates file

--- terre00.py
+++ terre00.py
@@ -4,7 +4,7 @@ letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
 
 def alphabet(letters_list):
     for n in letters_list:
-        print(n)
+        print(n, end=" ")
 
 
 alphabet(letters)
