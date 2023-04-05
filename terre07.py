9c0b8b63bfd7e70960ba1608f68277de7fd56440
EnesBrt <enes.barut@gmx.com>
2023-04-05 10:31:41 +0200

    add terre07.py file

---
+++ terre07.py
@@ -0,0 +1,19 @@
+# Taille d’une chaîne
+
+import sys
+
+
+def len_string(string):
+    print(len(string))
+
+
+if len(sys.argv) > 2:
+    print("Erreur : Veuillez fournir un seul message en tant qu'argument.")
+elif len(sys.argv) == 2:
+    message = sys.argv[1]
+    if message.isdigit():
+        print("Erreur : Veuillez fournir une chaîne de caractères non numérique en tant qu'argument.")
+    else:
+        len_string(message)
+else:
+    print("Erreur : Veuillez fournir un message en tant qu'argument.")
