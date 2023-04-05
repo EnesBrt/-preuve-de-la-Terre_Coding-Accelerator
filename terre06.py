1983d500cce114d03bfc2f7defdc09f50ceabb2a
EnesBrt <enes.barut@gmx.com>
2023-04-05 10:20:15 +0200

    update terre06.py file

--- terre06.py
+++ terre06.py
@@ -1,12 +1,16 @@
 # Inverser une chaîne
 
+import sys
+
+
 def inverse(words):
     print(words[::-1])
 
-    if not words:
-        print("erreur.")
 
-
-word = input()
-
-inverse(word)
+if len(sys.argv) > 2:
+    print("Erreur : Veuillez fournir un seul message en tant qu'argument.")
+elif len(sys.argv) == 2:
+    message = sys.argv[1]
+    inverse(message)
+else:
+    print("Erreur : Veuillez fournir un message en tant qu'argument.")
