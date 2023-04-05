6355493bb2dd9810d3eb147ff49690a39b5811e0
EnesBrt <enes.barut@gmx.com>
2023-04-05 10:49:38 +0200

    add terre08.py file

---
+++ terre08.py
@@ -0,0 +1,23 @@
+# Puissance d’un nombre
+
+import sys
+
+
+def power(a, b):
+    a = int(a)
+    b = int(b)
+    result = a ** b
+    print(result)
+
+
+if len(sys.argv) > 3:
+    print("Erreur : Veuillez fournir deux nombres en tant qu'argument.")
+elif len(sys.argv) == 3:
+    number_one = sys.argv[1]
+    number_two = sys.argv[2]
+    if number_one.isalpha() and number_two.isalpha():
+        print("Erreur : Veuillez fournir une chaîne de caractères numérique en tant qu'argument.")
+    else:
+        power(number_one, number_two)
+else:
+    print("Erreur : Veuillez fournir un message en tant qu'argument.")
