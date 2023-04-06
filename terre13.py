48a8948baae7008d8a6df6c8cd7d078c02526335
EnesBrt <enes.barut@gmx.com>
2023-04-06 13:54:30 +0200

    added terre13.py

---
+++ terre13.py
@@ -0,0 +1,55 @@
+# Trouver la Suisse
+
+import sys
+
+
+def find_the_middle(a, b, c):
+    a = int(a)
+    b = int(b)
+    c = int(c)
+
+    numbers = [a, b, c]
+
+    minimum = numbers[0]
+    maximum = numbers[0]
+    number = 0
+
+    equal = False
+    for n in range(1, len(numbers)):
+        if numbers[n] == numbers[0]:
+            equal = True
+            break
+
+    if equal:
+        print("erreur.")
+
+    for n in numbers:
+        if n < minimum:
+            minimum = n
+
+        if n > maximum:
+            maximum = n
+
+    numbers.remove(minimum)
+    numbers.remove(maximum)
+
+    for x in numbers:
+        if equal:
+            break
+        else:
+            number += x
+            print(number)
+
+
+if len(sys.argv) > 4:
+    print("Erreur: Veuillez fournir trois nombres en tant qu'argument.")
+elif len(sys.argv) == 4:
+    number_one = sys.argv[1]
+    number_two = sys.argv[2]
+    number_three = sys.argv[3]
+    if number_one.isalpha() and number_two.isalpha():
+        print("Erreur: Veuillez fournir trois nombres en tant qu'argument.")
+    else:
+        find_the_middle(number_one, number_two, number_three)
+else:
+    print("Erreur: Veuillez fournir trois nombres en tant qu'argument.")
